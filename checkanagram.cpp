7656558b40859760c8b77756b13a7c4a69a6035b
Raymond Linn <rlinn@alpine-la.com>
2016-03-03 13:15:09 -0800

    added to check white space and convert to lower case

--- checkanagram.cpp
+++ checkanagram.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <assert.h>
 
 using namespace std;
+
+char *removeSpaceAndConvertToLowerCase(char *input) {
+	
+	assert(input != NULL);
+	char *output = new char [strlen(input) + 1];
+	char *ip = input, *op = output;
+	int i = 0, j = 0;
+
+	while (ip[i] != '\0') {
+		if (ip[i] != ' ') {
+			op[j] = ip[i];
+			if (op[j] >= 'A' && op[j] <= 'Z') {
+				op[j] = op[j] + 32;
+			}
+			j++;
+		}
+		i++;
+	}
+	op[j] = '\0';
+	return output;	
+}
 
 bool checkAnagram(char str1[], char str2[]){
 
@@ -10,31 +32,32 @@ bool checkAnagram(char str1[], char str2[]){
 	int second[26] = {0};
 	int cnt = 0;
 
-	// take care of space and punctuation and convert to lower case
-
-	// count how many times that a character appears 
-
-	while (str1[cnt] != 0) {
-		first[str1[cnt] - 'a']++;
-		cnt++;
+	char *out1 = removeSpaceAndConvertToLowerCase(str1);
+	char *out2 = removeSpaceAndConvertToLowerCase(str2);
+	while (out1[cnt] != 0) {		
+		first[out1[cnt] - 'a']++;
+		cnt++;			
 	}
-
 	cnt = 0;
 
-	while (str2[cnt] != 0){
-		second[str2[cnt] - 'a']++;
+	while (out2[cnt] != 0){
+		second[out2[cnt] - 'a']++;
 		cnt++;
 	}
 
-	for (cnt = 0; cnt < 26; cnt++) {
-		std::cout << cnt << ": " << first[cnt] << " and " << second[cnt] << std::endl;
-		
-		if (first[cnt] != second[cnt])
+	for (cnt = 0; cnt < 26; cnt++) {		
+		if (first[cnt] != second[cnt]) {
+			delete [] out1;
+			delete [] out2;
 			return false;
+		}
 	}
+	delete [] out1;
+	delete [] out2;
 	return true;
 }
 
+/*
 void checkAnagram2(string a, string b) {
 
 	if(a.size() != b.size()){
@@ -42,7 +65,7 @@ void checkAnagram2(string a, string b) {
 		return;
 	}
    	// move the space and punctuation and change the case	
- 	static const auto space_or_punct =( char c )[]{ return std::ispunct(c) || std::isspace(c) ; } ;
+ 	static const auto space_or_punct = (char c)[] { return std::ispunct(c) || std::isspace(c) ; } ;
     a.erase(std::remove_if(a.begin(), a.end(), space_or_punct), a.end());
     for (char& c : a) c = std::tolower(c);
 
@@ -53,22 +76,16 @@ void checkAnagram2(string a, string b) {
     std::sort(b.begin(),b.end());
     std::cout<<"They are "<<((a==b)?"":"not")<<" anagrams." << std::endl;
 }
-
+*/
 int main() {
 
-	char *str1 = "A";
-	int first[26] = {0};
-
-	//std::cout << "a is: " << 'a' << std::endl;
-	//std::cout << "str1[1] is: " << str1[0] << std::endl;
-	//std::cout << "first[str1[0]- a] is: " << first[str1[0]- 'a']++ << std::endl;
-/*
+	
 	if (checkAnagram("nannyoldrim", "Raymond Linn") == true){
 		std::cout << "the two strings are anagrams" << std::endl;
 	} else {
 		std::cout << "the two strings are not anagrams" << std::endl;
 	}
-*/
-	checkAnagram2("nannyoldrim", "Raymond Linn");
+	
+	//checkAnagram2("nannyoldrim", "Raymond Linn");
 	return 0;
 }
