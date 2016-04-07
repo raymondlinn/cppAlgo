9d76e36e535f1268733eb2a0554e70f027e99475
raymondlinn <raymondlinn@gmail.com>
2016-04-06 20:32:07 -0700

    added .gitignore, modified stringreverse.cpp and mergesort.cpp

--- stringreverse.cpp
+++ stringreverse.cpp
@@ -1,35 +1,59 @@
 #include <iostream>
 
-char *strReverseInPointers(char *orgStr) {
-
-	int i = 0;
-	while(*orgStr){
-		*orgStr++;
-		i++;
-	}
-	// allocate the reverseStr
-	char *reverseStr = new char[i];
-	while (i >= 0) {
-		*orgStr--;
-		reverseStr = orgStr;
-		reverseStr++;
-		--i;
-	}
+using namespace std;
+ 
+int string_length(char*);
+void reverse(char*);
+ 
+int main() 
+{
 	
-	return reverseStr;
+   	char string[100];
+ 
+   	cout << "Enter a string you want to reverse: " << endl;
+   
+ 	cin >> string;
+ 
+   reverse(string);
+ 
+   cout << "Reverse of entered string is: " <<  string << endl;
+ 
+   return 0;
+}
+ 
+void reverse(char *string) 
+{
+	cout << "reverse: string is: " << string << endl;
+
+   	int length, c;
+   	char *begin, *end, temp;
+ 
+   	length = string_length(string);
+ 
+   	begin = string;
+   	end = string;
+ 
+   	for ( c = 0 ; c < ( length - 1 ) ; c++ )
+      	end++;
+ 
+   	for ( c = 0 ; c < length/2 ; c++ ) 
+   	{        
+      	temp = *end;
+      	*end = *begin;
+      	*begin = temp;
+ 
+      	begin++;
+      	end--;
+   	}
+}
+ 
+int string_length(char *pointer)
+{
+   	int c = 0;
+ 
+  	while( *(pointer+c) != '\0' )
+      	c++;
+ 
+   	return c;
 }
 
-int main() {
-
-	char *orgStr = "happyz";
-	char *revStr;
-
-	//std::cout << "Enter any String to be reversed:" << std::endl;
-	//std::cin >> orgStr;
-	revStr = strReverseInPointers(orgStr);
-	std::cout << "Reversed String is: " << revStr << std::endl;
-
-	delete [] revStr;
-
-	return 0;
-}
